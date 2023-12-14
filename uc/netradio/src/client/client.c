5ba351d7b0af8f377f920cad00e62faf6dd65559
hakele <2281752708@qq.com>
2023-12-14 20:39:10 +0800

    code

---
+++ uc/netradio/src/client/client.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct client_conf_St client_conf = 
+{
+    .rcvport = DEFAULT_RCVPORT,
+    .mgroup = DEFAULT_MGROUP,
+
+
+};
+int main (int argv, char **argv)
+{
+    getopt_long ();
+    socket
+
+    pipe ();
+    
+    fork ();
+
+    return 0;
+}
---
+++ uc/netradio/src/client/client.h
@@ -0,0 +1,13 @@
+#ifndef __CLIENT_H__
+#define _CLIENT_H__
+#define DEFAULT_PLAYERCMD  ""
+
+struct client_conf_st
+{
+    char *rcvport;
+    char *mgroup;
+    char *player_cmd;
+
+};
+
+#endif
